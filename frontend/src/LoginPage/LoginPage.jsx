3aa5d403d14c290cba4ec77df6830a0584cce92f
nicolae <stroncea.nicolae@gmail.com>
2020-02-27 13:08:15 -0500

    login

--- frontend/src/LoginPage/LoginPage.jsx
+++ frontend/src/LoginPage/LoginPage.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { FormControl } from '@material-ui/core';
+import './LoginPage.css'
 
 class LoginPage extends React.Component {
   constructor(props) {
@@ -13,15 +17,25 @@ class LoginPage extends React.Component {
     };
   }
 
+
   render() {
-      
-      return (
-        <div className="Login">
-          <div className="container">
-              Login
-              </div>
-          </div>
-      );
+    return (
+      <div className="container">
+        <div className="login">
+          <FormControl className="form">
+            <TextField id="email"  required placeholder="email" variant="outlined" />
+            <TextField id="password"  type="password" autoComplete="current-password" required placeholder="password" variant="outlined" />
+            <Button variant="contained" color="primary"  value="Submit" >
+                LogIn
+            </Button>
+            <div>Not registered yet?</div>
+            <Button variant="contained" color="primary" value="Submit">
+            Register
+            </Button>
+          </FormControl>
+        </div>
+      </div>
+    );
   }
 }
 
