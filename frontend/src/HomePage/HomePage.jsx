e0619c5dcc1f1cf4fba926b20eaaf8a4d23fef78
Wenqin Ye <wenqinye@Wenqins-MacBook-Pro.local>
2020-02-27 13:27:17 -0500

    added admin page

--- frontend/src/HomePage/HomePage.jsx
+++ frontend/src/HomePage/HomePage.jsx
@@ -1,26 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-
-const useStyles = makeStyles(theme => ({
-    root: {
-      flexGrow: 1,
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
-    title: {
-      flexGrow: 1,
-    },
-  }));
-
 class HomePage extends React.Component {
 
   render() {
