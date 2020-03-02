dcca3eee6c23ff4593765724bd38f4d86e68bf5c
nicolae <stroncea.nicolae@gmail.com>
2020-03-01 23:34:02 -0500

    added date and type to fake data object

--- frontend/src/_services/UserService.js
+++ frontend/src/_services/UserService.js
@@ -1,14 +1,3 @@
-//fake data
-const data = [{user: 'admin', title: 'Beach Cleanup', desc: 'Clean the beach of ontario!'}, 
-{user: 'admin', title: 'Teach Art and Paint Murals', desc: 'Provide art classes to the youth and help with community painting projects'},
-{user: 'admin', title: 'Teach English Classes', desc: 'Teach English to children and adults through short-term intensive classes'},
-{user: 'admin', title: 'Provide Child-Care Support', desc: 'Work with young children and provide support to day-care staff'},
-{user: 'admin', title: 'Support Manual Labor Projects', desc: 'Help paint homes and support basic manual labor tasks for local residents'},
-{user: 'past', title: 'Clean the beaches', desc: 'Help clean the beach at Harbourfront'},
-{user: 'past', title: 'Clean the oceans', desc: 'Clean the Atlantic Ocean'},
-{user: 'past', title: 'Volunteer at SOS Autism', desc: 'Translate grant application documents'},
-{user: 'future', title: 'Plant trees in HighGarden', desc: 'Bring a shovel!'}
-]
 // include all  user database calls and all functions here 
 
 // generate random date for fake data
@@ -21,8 +10,21 @@ const randomDate = (start, end) => {
     month = month.length < 2 ? '0' + month : month;
     day = day.length < 2 ? '0' + day : day;
 
-    return [year, month, day].join('-');
+	return [year, month, day].join('-');
 }
+
+//fake data
+const data = [{user: 'admin', title: 'Beach Cleanup', desc: 'Clean the beach of ontario!',type:'Clean Up', date:randomDate(new Date(2020, 0, 1), new Date())}, 
+{user: 'admin', title: 'Teach Art and Paint Murals', desc: 'Provide art classes to the youth and help with community painting projects', type:'Community Building', date:randomDate(new Date(2020, 0, 1), new Date())},
+{user: 'admin', title: 'Teach English Classes', desc: 'Teach English to children and adults through short-term intensive classes', type:'Community Building', date:randomDate(new Date(2020, 0, 1), new Date())},
+{user: 'admin', title: 'Provide Child-Care Support', desc: 'Work with young children and provide support to day-care staff',type:'Community Building', date:randomDate(new Date(2020, 0, 1), new Date())},
+{user: 'admin', title: 'Support Manual Labor Projects', desc: 'Help paint homes and support basic manual labor tasks for local residents',type:'Community Building', date: randomDate(new Date(2020, 0, 1), new Date())},
+{user: 'past', title: 'Clean the beaches', desc: 'Help clean the beach at Harbourfront', type:'Clean Up', date: randomDate(new Date(2020, 0, 1), new Date())},
+{user: 'past', title: 'Clean the oceans', desc: 'Clean the Atlantic Ocean', type:'Clean Up', date: randomDate(new Date(2020, 0, 1), new Date())},
+{user: 'past', title: 'Volunteer at SOS Autism', desc: 'Translate grant application documents', type:'Community Building', date: randomDate(new Date(2020, 0, 1), new Date())},
+{user: 'future', title: 'Plant trees in HighGarden', desc: 'Bring a shovel!', type:'Planting', date: randomDate(new Date(2020, 0, 1), new Date())}
+]
+
 const getEvents = (user) => {
 	for(let i = 0; i < data.length; i++){
 		data[i].date = randomDate(new Date(2020, 0, 1), new Date())
