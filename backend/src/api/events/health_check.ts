9511c2f547548c0c2645c5f4fd0052a0426e42d7
Simran <snike2610@gmail.com>
2020-03-23 18:16:28 -0400

    refactor

---
+++ backend/src/api/events/health_check.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from "express";
+import { db } from "../../db";
+
+export const router = express.Router();
+
+const health_check = async (req:Request , res: Response) =>  {
+  console.log("hereerre");
+  const query = { "sqlStatement": "SELECT * FROM events" }
+  const filler = "none";
+  const result = await db.db_query(query, filler);
+  res.send(result);
+  // res.send('Hello World!');
+};
+
+
+router.use("/", health_check);
