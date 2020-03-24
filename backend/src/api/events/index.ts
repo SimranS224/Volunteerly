ea4ceb0d69ca1e0b272227a65aee7af5ef353937
nicolae <stroncea.nicolae@gmail.com>
2020-03-23 23:25:14 -0400

    sequelize start

--- backend/src/api/events/index.ts
+++ backend/src/api/events/index.ts
@@ -5,6 +5,9 @@ import bodyParser from "body-parser";
 import compression from "compression";
 import morgan from "morgan";
 import cors from "cors";
+import {Sequelize} from 'sequelize-typescript';
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 
@@ -12,7 +15,26 @@ app.use(cors());
 app.use(morgan("combined"));
 app.use(compression());
 app.use(bodyParser.json());
-
 app.use("/health_check", health_check);
+const get_sequelize = () => {
+    if(process.env.NODE_ENV === "production"){
+        // return new Sequelize(process.env.DATABASE_URL);
+        return new Sequelize(process.env.DATABASE_URL,{operatorsAliases: false});
+    }
+    else{
+        // return new Sequelize(process.env.DEV_DATABASE_URL);
+        return new Sequelize(process.env.DEV_DATABASE_URL,{operatorsAliases: false});
+    }
+}
+// sequelize.authenticate().then(() => {
+//   console.log('Connection established successfully.');
+// }).catch(err => {
+//   console.error('Unable to connect to the database:', err);
+// })
+const sequelize = get_sequelize();
+//TODO add all of the models
+//TODO how to make sure this is run before everything else?
+const handler = serverless(app);
+export {handler, sequelize};
 
-export const handler = serverless(app);
+ 
