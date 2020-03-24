6823ae6385ee096cc6d7428ccc63408f59b56f1f
Simran <snike2610@gmail.com>
2020-03-23 23:45:52 -0400

    fixed deployment

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -1,7 +1,6 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-
 const AWS = require('aws-sdk')
 const RDS = new AWS.RDSDataService()
 
@@ -16,15 +15,21 @@ const db_query = async (event, context) => {
   console.log({sqlStatement});
   
   // The Lambda environment variables for the Aurora Cluster Arn, Database Name, and the AWS Secrets Arn hosting the master credentials of the serverless db
-  var DBSecretsStoreArn = process.env.DBSecretsStoreArn;
-  var DBAuroraClusterArn = process.env.DBAuroraClusterArn;
-  var DatabaseName = process.env.DatabaseName;
+  var secretArn = process.env.secretArn;
+  var resourceArn = process.env.resourceArn;
+  var DatabaseName = process.env.database;
+  console.log({DatabaseName});
+  
   const params = {
-    secretArn: DBSecretsStoreArn,
-    resourceArn: DBAuroraClusterArn,
-    sql: sqlStatement,
-    database: DatabaseName
-  }
+    "secretArn": secretArn,
+    "resourceArn": resourceArn,
+    "sql": sqlStatement,
+    "database": DatabaseName
+  };
+
+  const x = 1;
+  console.log({params});
+  
   try {
     let dbResponse = await RDS.executeStatement(params).promise()
     console.log(JSON.stringify(dbResponse, null, 2))
