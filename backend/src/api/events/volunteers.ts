c8218c9f02321301837f55bd1b690442847c4596
nicolae <stroncea.nicolae@gmail.com>
2020-03-25 18:07:54 -0400

    post_volunteers semi working

--- backend/src/api/events/volunteers.ts
+++ backend/src/api/events/volunteers.ts
@@ -4,13 +4,17 @@ import {sequelize} from "../../db"
 // export const router = express.Router();
 
 const post_volunteers = async (req:Request , res: Response) =>  {
-    console.log("running volunteers")
-    sequelize.sync().then(()=> Volunteer.create({name:'Test volunteer',email:'test', password:'another', profile_picture_url:'test'}));
+    console.log("request is:", req)
+    console.log("posting volunteers")
+    const {data} = req.body;
+    console.log("request body", data)
+    // sequelize.sync().then(()=> Volunteer.create({name:'Test volunteer',email:'test', password:'another', profile_picture_url:'test'}));
+    sequelize.sync().then(()=>Volunteer.create(data)).then(response => res.send(response))
   
 }
 
 const get_volunteers = async (req:Request , res: Response) =>  {
-    console.log("running volunteers")
+    console.log("getting volunteers")
     sequelize.sync().then(()=> Volunteer.findAll()).then(Volunteer=>console.log(Volunteer))
   
 }
