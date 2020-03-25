05207b1105cd8e274820a16eb063daccdcac687f
nicolae <stroncea.nicolae@gmail.com>
2020-03-25 19:12:47 -0400

    post and get volunteer working calls

--- backend/src/api/events/volunteers.ts
+++ backend/src/api/events/volunteers.ts
@@ -10,14 +10,13 @@ const post_volunteers = async (req:Request , res: Response) =>  {
     console.log("posting volunteers")
     const data = req.body;
     console.log({data})
-    // sequelize.sync().then(()=> Volunteer.create({name:'Test volunteer',email:'test', password:'another', profile_picture_url:'test'}));
-    sequelize.sync().then(()=>Volunteer.create(data)).then(response => res.send(response))
-  
+    // sequelize.sync().then(()=>Volunteer.create(data)).then(response => res.send(response))
+    sequelize.sync().then(() => Volunteer.create(data)).then(newVolunteer => res.send(newVolunteer))
 }
 
 const get_volunteers = async (req:Request , res: Response) =>  {
     console.log("getting volunteers")
-    sequelize.sync().then(()=> Volunteer.findAll()).then(Volunteer=>console.log(Volunteer))
+    sequelize.sync().then(()=>Volunteer.findAll()).then((users)=>res.send(users))
   
 }
 
