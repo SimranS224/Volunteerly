18a198b194ea5497a022bf4b86d5abfa8f6c4b34
Simran <snike2610@gmail.com>
2020-03-26 14:32:39 -0400

    adding stats route

--- backend/src/api/index.ts
+++ backend/src/api/index.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import serverless from "serverless-http";
-import { router as login} from "./login";
+import { router as login } from "./login";
 import { router as volunteers } from "./volunteers/new_index";
-import { router as db_calls} from "./database_events";
+import { router as db_calls } from "./database_events";
+import { router as stats } from "./stats";
 import bodyParser from "body-parser";
 import compression from "compression";
 import morgan from "morgan";
@@ -18,9 +19,9 @@ app.use(morgan("combined"));
 app.use(compression());
 app.use(bodyParser.json());
 
-
 app.use("/*/volunteers", volunteers)
 app.use("/*/database", db_calls)
 app.use("/*/login", login)
+app.use("/*/achievements", stats)
 
 export const handler = serverless(app)
