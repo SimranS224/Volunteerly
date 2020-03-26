9313a75454c2f097d6fe52f08e5bae987bbb7f96
Carlson Lau <carl.lau999@gmail.com>
2020-03-26 05:23:22 -0400

    send profile pic to S3 upon registering

--- frontend/src/RegisterPage/RegisterPage.jsx
+++ frontend/src/RegisterPage/RegisterPage.jsx
@@ -6,6 +6,15 @@ import Button from '@material-ui/core/Button';
 import { FormControl } from '@material-ui/core';
 import { userActions } from "../_redux/_actions";
 import './RegisterPage.css'
+import ImageUploader from "react-images-upload";
+import S3 from 'aws-s3';
+const config = {
+    bucketName: 'volunteer-app-images',
+    region: 'ca-central-1',
+    accessKeyId: 'AKIA2ZJCVHTCYEEVWUKV',
+    secretAccessKey: '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',
+}
+const S3Client = new S3(config);
 import store from 'store'
 class RegisterPage extends React.Component {
   constructor(props) {
@@ -15,13 +24,30 @@ class RegisterPage extends React.Component {
         email: '',
         password: '',
         first_name: '',
-        last_name: ''
+        last_name: '',
+        pictures: []
     };
   }
+  uploadPhoto = async () => {
+    try{
+    const data = await S3Client.uploadFile(this.state.pictures[0], this.state.email + "-profile")
+    console.log(data)
+    return data
+    }
+    catch(err){
+      console.log(err)
+    }
+
+  }
   register = async (first_name, last_name, email, password) =>{
+    let profile_picture_url = ""
+    if(this.state.pictures.length > 0){
+      let data = await this.uploadPhoto()
+      profile_picture_url = data.location
+    }
     let res = await fetch('http://localhost:3004/dev/api/volunteers/', {
         method: 'post',
-        body:  JSON.stringify({first_name: first_name, last_name: last_name, email: email, password: password}),
+        body:  JSON.stringify({first_name: first_name, last_name: last_name, email: email, password: password, profile_picture_url: profile_picture_url}),
         headers: { 'Content-Type': 'application/json' },
     })
     res = await res.json()
@@ -33,7 +59,12 @@ class RegisterPage extends React.Component {
     }
 
   }
-
+onDrop = (pictureFiles) =>{
+        console.log(pictureFiles)
+        this.setState({
+            pictures: this.state.pictures.concat(pictureFiles)
+        });
+    }
   render() {
     return (
       <div className="container">
@@ -45,6 +76,14 @@ class RegisterPage extends React.Component {
 
             <TextField id="email"  required placeholder="Email" variant="outlined" onChange={(e)=> {this.setState({'email': e.target.value})}}/>
             <TextField id="password" required type="Password" onChange={(e)=> {this.setState({'password': e.target.value})}} autoComplete="current-password" required placeholder="password" variant="outlined" />
+            <ImageUploader
+                            withIcon={true}
+                            withPreview={true}
+                            buttonText="Choose a profile picture!"
+                            onChange={this.onDrop.bind(this)}
+                            imgExtension={[".jpg", ".gif", ".png", ".jpeg", ".JPG", ".GIF", ".PNG", ".JPEG"]}
+                            maxFileSize={5242880}
+                          />
             <Button variant="contained" color="primary"  value="Submit" onClick={() => {
               this.register(this.state.first_name, this.state.last_name, this.state.email, this.state.password)
             }}>
