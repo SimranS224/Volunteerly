c84d39286f00db65a3f9d28271475f926f4012ae
Simran <snike2610@gmail.com>
2020-03-25 23:19:18 -0400

    refactor volunteer endpoint

--- backend/src/api/events/index.ts
+++ backend/src/api/events/index.ts
@@ -1,23 +1,21 @@
 import express from "express";
 import serverless from "serverless-http";
-import { router as events } from "./events";
-import { router as health_check } from "./health_check";
 import { router as volunteers } from "./volunteers";
 import bodyParser from "body-parser";
 import compression from "compression";
 import morgan from "morgan";
 import cors from "cors";
+
 import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
+
 app.use(cors());
 app.use(morgan("combined"));
 app.use(compression());
 app.use(bodyParser.json());
 
-// app.use("/health_check", health_check);
-// app.use("/events", events);
 app.use("/volunteers", volunteers)
 
 export const handler = serverless(app)
