4e827a95332ad093c5992493d49e641484ac6802
Carlson Lau <carl.lau999@gmail.com>
2020-03-25 23:45:22 -0400

    check for email before adding volunteer

--- backend/src/api/events/volunteers.ts
+++ backend/src/api/events/volunteers.ts
@@ -1,6 +1,9 @@
 import express, { Request, Response } from "express";
 import {Volunteer} from "../../db/models"
 import {sequelize} from "../../db"
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+
 
 export const router = express.Router();
 
@@ -8,10 +11,26 @@ export const router = express.Router();
 const post_volunteers = async (req:Request , res: Response) =>  {
     // console.log("request is:", req)
     console.log("posting volunteers")
-    const data = req.body;
-    console.log({data})
+    const {email, name, password} = req.body;
+    console.log(req.body)
+    const hashedPassword = bcrypt.hashSync(password, 8);
+    console.log("hash", hashedPassword)
     // sequelize.sync().then(()=>Volunteer.create(data)).then(response => res.send(response))
-    sequelize.sync().then(() => Volunteer.create(data)).then(newVolunteer => res.send(newVolunteer))
+    await sequelize.sync()
+        
+    const users = await Volunteer.findAll()
+    for(let i = 0; i < users.length; i++){
+        if(users[i].email == email){
+            console.log("found existing email")
+            return res.send({status: 'error', msg: "Account with email already exists"})
+        }
+    }
+    const newVolunteer = await Volunteer.create({email: email, name: name, password: hashedPassword})
+    console.log(newVolunteer)
+    return res.send({status: "success", msg: ""})
+
+        
+        
 }
 
 const get_volunteers = async (req:Request , res: Response) =>  {
